b3368c146f8ce26a3c75627f34b930f3598cb025
Carlos Casas <albertocasas05@gmail.com>
2016-09-21 15:01:16 -0700

    MAybe

--- chatbot.js
+++ chatbot.js
@@ -9,3 +9,13 @@ var questionConversion = {
   "what is your name?":name,
   "what time is it?":is
 }
+
+if(questionConversion == old) {
+  $("#chat-area").html("I was born in Sept 21, 16.");
+}
+else if (questionConversion == name) {
+  $("#chat-area").html("My namme is Lex");
+}
+else {
+  $("#chat-area").html();
+}
