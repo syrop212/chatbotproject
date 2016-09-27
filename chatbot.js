19f824e6e0b0e4bcdfb20238a95c46bb5548c2e3
Carlos Casas <albertocasas05@gmail.com>
2016-09-27 15:00:28 -0700

    fallback reponse is on

--- chatbot.js
+++ chatbot.js
@@ -4,8 +4,12 @@ var answer;
 var questionConversion = {
   "how old are you?": "I was created, not born",
   "what is your name?": "Lex",
-  "what time is it?": "It is " + " " +
+  //"what time is it?": "It is " + " " +
 }
+
+var myNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var rand = Math.floor((Math.floor() * myNumbers.length));
 
 
 function talkBot() {
@@ -15,11 +19,10 @@ function talkBot() {
 
   answer = questionConversion[question];
 
+  if(answer === undefined) {
+    $("#chat-area").prepend("What is it again?" + "<br>");
+  }
+else {
   $("#chat-area").prepend(answer + "<br>");
-
-}
-
-
-function digitized() {
-  
+  }
 }
